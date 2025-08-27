a93febfa1b55055ec3ab25f13088e8e5d8916ea0
kinyalinah <kinyalinah6@gmail.com>
2025-08-27 12:07:57 +0300

    UPDATED MY JAVA

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-let londonTime = document.getElementById("london");
-let londonDates = londonTime.querySelector(".dates");
-londonDates.innerHTML = "Happy coding";
+function updateCity(cityID, timezone) {
+  let city = document.getElementById(cityID);
+  let dates = city.querySelector(".dates");
+  let time = city.querySelector(".time");
+  dates.innerHTML = moment().tz(timezone).format("DD MMMM YYYY");
+  time.innerHTML = moment()
+    .tz(timezone)
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+function updateAllCities() {
+  updateCity("london", "Europe/London");
+  updateCity("lagos", "Africa/Lagos");
+  updateCity("kampala", "Africa/kampala");
+}
+updateAllCities();
+
+setInterval(updateAllCities, 1000);
