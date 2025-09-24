f29509c1100011d504522b4b018bb953793bdfcf
kinyalinah <kinyalinah6@gmail.com>
2025-09-24 12:51:17 +0300

    updated java

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function updateCity(cityID, timezone) {
+function updateTime(cityID, timezone) {
   let city = document.getElementById(cityID);
 
   let dates = city.querySelector(".dates");
@@ -9,10 +9,34 @@ function updateCity(cityID, timezone) {
     .format("hh:mm:ss [<small>]A[</small>]");
 }
 function updateAllCities() {
-  updateCity("london", "Europe/London");
-  updateCity("lagos", "Africa/Lagos");
-  updateCity("kampala", "Africa/kampala");
+  updateTime("london", "Europe/London");
+  updateTime("paris", "Europe/Paris");
+  updateTime("kampala", "Africa/Kampala");
 }
 updateAllCities();
 
 setInterval(updateAllCities, 1000);
+
+function updateCity(event) {
+  let cityTime = event.target.value;
+
+  let cityElement = document.getElementById("cities-container");
+  if (!cityTime) {
+    cityElement.innerHTML = "";
+    return;
+  }
+
+  cityElement.innerHTML = `<div class="city-options" >
+        <div>
+          <h2>${cityTime}</h2>
+          <div class="dates">${moment()
+            .tz(cityTime)
+            .format("DD MMMM YYYY")}</div>
+          </div>
+          <div class="time">${moment()
+            .tz(cityTime)
+            .format("hh:mm:ss [<small>]A[</small>]")}</div>
+          </div>`;
+}
+let citiesSelect = document.getElementById("city");
+citiesSelect.addEventListener("change", updateCity);
